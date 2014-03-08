9af667e4e0fa40b532382501b6a600c21baad540
James Smith <metal.lunchbox@gmail.com>
2014-03-09 03:35:30 +0800

    adding the models and admin settings for the basic functions

--- learntron/admin.py
+++ learntron/admin.py
@@ -1,3 +1,72 @@
 from django.contrib import admin
+from learntron.models import Lesson, Slide, Poll, Choice, Answer, Note
 
-# Register your models here.
+
+class SlideInline(admin.StackedInline):
+    model = Slide
+    extra = 10
+
+
+class LessonAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_time'
+    exclude = ('created_time', 'created_by', 'modified_by')
+    list_display = ('title', 'created_time', 'created_by', 'last_edited',
+                    'modified_by')
+    inlines = [SlideInline]
+    save_on_top = True
+
+    def save_model(self, request, instance, form, change):
+        user = request.user
+        instance = form.save(commit=False)
+        if not change or not instance.created_by:
+            instance.created_by = user
+        instance.modified_by = user
+        instance.save()
+        form.save_m2m()
+        return instance
+
+
+class SlideAdmin(admin.ModelAdmin):
+    list_display = ('_order', 'lesson', '__unicode__')
+    list_filter = ('lesson',)
+    ordering = ['lesson', '_order']
+
+
+class NoteAdmin(admin.ModelAdmin):
+    exclude = ('created_time', 'created_by', 'modified_by')
+    list_display = ('lesson', '__unicode__')
+
+    def save_model(self, request, instance, form, change):
+        user = request.user
+        instance = form.save(commit=False)
+        if not change or not instance.created_by:
+            instance.created_by = user
+        instance.modified_by = user
+        instance.save()
+        form.save_m2m()
+        return instance
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text', 'poll')
+    list_filter = ('poll',)
+
+
+class PollAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('choice', 'poll', 'user', 'timestamp')
+
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Slide, SlideAdmin)
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Answer, AnswerAdmin)
--- learntron/models.py
+++ learntron/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=40, unique=True)
+    created_by = models.ForeignKey(User, related_name='lesson_createdby')
+    modified_by = models.ForeignKey(User, related_name='lesson_modifiedby')
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['created_time']
+
+
+class Note(models.Model):
+    lesson = models.ForeignKey(Lesson)
+    content = models.TextField()
+    created_by = models.ForeignKey(User, related_name='note_createdby')
+    modified_by = models.ForeignKey(User, related_name='note_modifiedby')
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        content_trunc = self.content[:85] + (self.content[81:] and '...')
+        return content_trunc
+
+
+class Slide(models.Model):
+    lesson = models.ForeignKey(Lesson)
+    content = models.TextField()
+
+    def __unicode__(self):
+        content_trunc = self.content[:65] + (self.content[61:] and '...')
+        return content_trunc
+
+    class Meta:
+        order_with_respect_to = "lesson"
+
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.choice_text
+
+
+class Answer(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice = models.ForeignKey(Choice)
+    user = models.ForeignKey(User)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.choice.choice_text
