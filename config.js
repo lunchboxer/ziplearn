2e9a7a544d708b9c58a023c997bc7a6cfedf1b24
James Smith <metal.lunchbox@gmail.com>
2014-03-18 19:41:29 +0800

    Set up the projec with a pretty standard Ember structure. Renamed REAME to .md. Got rid of grunt, using Brunch instead, way simpler and faster. The passkey.json file is for ember.vim. Also not using bower. Package managment is necessary when you have hundreds or thousands of packages that might be updated daily, but for three or four javascripts I don't think it's neccessary. Installing the scripts by hand gives more control over which scripts we have, where we put them, and what they are named.

---
+++ config.js
@@ -0,0 +1,63 @@
+exports.config = {
+  paths: {
+    watched: ['app', 'vendor', 'test']
+  },
+  files: {
+    javascripts: {
+      joinTo: {
+        'javascripts/app.js': /^app/,
+        'javascripts/vendor.js': /^(vendor\/scripts\/(common|development)|vendor\\scripts\\(common|development))/
+      },
+      order: {
+        before: [
+          'vendor/scripts/common/jquery.js',
+          'vendor/scripts/common/handlebars.js',
+          'vendor/scripts/development/ember.js',
+          'vendor/scripts/development/ember-data.js'
+        ]
+      }
+    },
+    stylesheets: {
+      defaultExtension: 'scss',
+      joinTo: {
+        'stylesheets/app.css': /^(app\/styles)/
+      }
+    },
+    templates: {
+      precompile: true,
+      root: 'templates',
+      joinTo: {
+        'javascripts/app.js': /^app/
+      }
+    }
+  },
+  //overrides: {
+
+    //// Production Settings
+    //production: {
+      //files: {
+        //javascripts: {
+          //joinTo: {
+            //'javascripts/app.js': /^app/,
+            //'javascripts/vendor.js': /^(vendor\/scripts\/(common|development)|vendor\\scripts\\(common|development))/
+          //},
+          //order: {
+            //before: [
+              //'vendor/scripts/common/jquery.js',
+              //'vendor/scripts/common/handlebars.js',
+              //'vendor/scripts/production/ember.js',
+              //'vendor/scripts/production/ember-data.js'
+            //]
+          //}
+        //}
+      //},
+      //optimize: true,
+      //sourceMaps: false,
+      //plugins: {
+        //autoReload: {
+          //enabled: false
+        //}
+      //}
+    //}
+  //}
+};
