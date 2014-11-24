b586682d7ad3c76871a4b01fcef879fad889e129
James Smith <metal.lunchbox@gmail.com>
2014-11-25 01:08:52 +0800

    adding roles for permissions. making router redirect for pages requiring login

--- server/controller.js
+++ server/controller.js
@@ -1,18 +1,18 @@
-controlStream = new Meteor.Stream('control');
+controlStream = new Meteor.Stream('control')
 
 controlStream.permissions.write(function() {
-  return true;
-});
+  return true
+})
 
 controlStream.permissions.read(function() {
-  return true;
-});
+  return true
+})
 
 controlStream.addFilter(function(eventName, args) {
   if (this.userId) {
-    var user = Meteor.users.findOne(this.userId);
-    return [args[0], user.username];
+    var user = Meteor.users.findOne(this.userId)
+    return [args[0], user.username]
   } else {
-    return args;
+    return args
   }
-});
+})
---
+++ server/fixture.js
@@ -0,0 +1,25 @@
+// create a test user on reset
+Meteor.startup(function() {
+  if (Meteor.users.find().count() === 0) {
+    Accounts.createUser({
+      username: 'james',
+      email: 'email@example.com',
+      password: 'password',
+      profile: {
+        first_name: 'James',
+        last_name: 'Smith'
+      }
+    });
+  }
+
+  if (!Meteor.roles.findOne({name: "controller"}))
+    Roles.createRole("controller");
+
+  if (!Meteor.roles.findOne({name: "admin"}))
+    Roles.createRole("admin");
+
+  adminUser = Meteor.users.findOne({
+    username: 'james'
+  })
+  Roles.addUsersToRoles(adminUser, ['admin', 'controller']);
+})
